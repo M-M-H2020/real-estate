64a5febefa590fe4e0c2ecd765a1d77391d81daa
Mohammed Mahmoud Hussein <mail.M.M.H2020@gmail.com>
2022-01-12 12:33:58 +0400

    Fix a bug

--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -3,12 +3,11 @@ import axios from 'axios'
 export const baseUrl = 'https://bayut.p.rapidapi.com'
 
 export const fetchApi = async url => {
-  const response = await axios.get(url, {
+  const { data } = await axios.get(url, {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': `${process.env.RAPID_API_KEY}`,
+      'x-rapidapi-key': `${process.env.NEXT_PUBLIC_RAPID_API_KEY}`,
     },
   })
-  const data = response.data
   return data
 }
